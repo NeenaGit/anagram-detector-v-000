a82f38f822fdc873c4757db3f046f7b12f5ef19f
Neena van de Water <nkuriakose@hotmail.com>
2018-03-07 17:34:39 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
     find_match_word = word.each { |w| w.split("").sort}
     # #compare each word with word initialized
     # same_letters = find_match_word.sort
-
+    binding.pry
 
     #are they composed of the same letters(use split to split words into letters)
     #use sort to compare letter in both arrays.
