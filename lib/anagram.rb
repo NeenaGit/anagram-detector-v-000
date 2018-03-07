96ae4cc4838f97e2443f5cac6fb4e5f3088e0702
Neena van de Water <nkuriakose@hotmail.com>
2018-03-07 17:25:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
 
   def match(word)
     #iterate over array of words
-    find_match_word = word.each { |l| l.split("")}
+    find_match_word = word.each { |l| l.scan("")}
     #compare each word with word initialized
     same_letters = find_match_word.sort
     binding.pry
