41f5e08c9c2529846eeffcb4c33ebc8f8329a6d3
Neena van de Water <nkuriakose@hotmail.com>
2018-03-07 17:32:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,16 +10,16 @@ class Anagram
 
   def match(word)
     #iterate over array of words
-    find_match_word = word.each { |w| w.split("")}
-    #compare each word with word initialized
-    same_letters = find_match_word.sort
-    
-    binding.pry
+    word.group_by { |element| element.downcase.chars.sort }.values
+    # find_match_word = word.each { |w| w.split("")}
+    # #compare each word with word initialized
+    # same_letters = find_match_word.sort
+
 
     #are they composed of the same letters(use split to split words into letters)
     #use sort to compare letter in both arrays.
     #compare 2 arrays with ==
-    same_letters
+    # same_letters
   end
 
 end
